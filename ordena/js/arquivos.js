b5759c5039a80c4b1c9f20c8abd00ad457eb6ace
Myllena <myllenahfernandes07@gmail.com>
2020-10-23 23:25:20 -0300

    ordena

---
+++ ordena/js/arquivos.js
@@ -0,0 +1,8 @@
+numeros = [7,37 ,95 ,17 ,18 ,24 ,30 ,13 ,85 ,6 ] ;
+
+    function  sortFunction (a ,b ) 
+    {
+        retorno(a -b);
+    }
+
+    console . log ( numeros . sort ( sortFunction ) ) ;
